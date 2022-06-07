cffa55e65f83363374c17266a8e1a9a0798c5927
Emmixe <maxchouraqui@yahoo.fr>
2022-06-07 11:09:23 +0200

    up

--- Front/map.js
+++ Front/map.js
@@ -48,7 +48,7 @@ function tab(fire) {
 }
 
 
-lfeu = new L.layerGroup();
+var lfeu = new L.layerGroup();
 map.addLayer(lfeu);
 
 function filtre_feu(){
@@ -190,7 +190,6 @@ function box_caserne(){
             coucheNotre_caserne();
             bnotre_caserne=true;
         }
-        
     }
 
     leurs_casernes.onchange = function(){
