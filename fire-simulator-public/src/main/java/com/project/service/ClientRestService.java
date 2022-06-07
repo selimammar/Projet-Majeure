8dc9a5fcf8c549aff4b5b5b420cf638b93784a83
OmarGass <gasriomar@gmail.com>
2022-06-07 16:29:31 +0200

    push

--- fire-simulator-public/src/main/java/com/project/service/ClientRestService.java
+++ fire-simulator-public/src/main/java/com/project/service/ClientRestService.java
@@ -1,14 +1,20 @@
 package com.project.service;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.BeanUtils;
 
 import com.project.model.dto.VehicleDTO;
+import com.project.model.Vehicle;
 
 @Service
 public class ClientRestService {
 	String url = "http://vps.cpe-sn.fr:8081/vehicle";
+	
+//	HttpHeaders headers = new HttpHeaders();
+//	headers.setContentType(MediaType.APPLICATION_JSON);
 	
     private final RestTemplate restTemplate;
 
@@ -24,9 +30,24 @@ public class ClientRestService {
         return this.restTemplate.getForObject(url, VehicleDTO[].class);
     }
     
-    // public VehicleDTO addVehicle(int teamuuid, VehicleDTO vehicle) {
-		
-    	
-    // }
+	private VehicleDTO vehicleToVehicleDTO(Vehicle vehicle){
+		VehicleDTO vehicleDTO = new VehicleDTO();
+		BeanUtils.copyProperties(vehicle, vehicleDTO);
+		return vehicleDTO;
+	}
+	
+	private Vehicle VehicleDTOTOVehicle(VehicleDTO vehicleDTO){
+		Vehicle vehicle = new Vehicle();
+		//Card card = new Card(cardDTO.getName(),cardDTO.getDescription(),cardDTO.getImgUrl(),cardDTO.getFamily(),cardDTO.getAffinity(),cardDTO.getHp(),cardDTO.getEnergy(),cardDTO.getAttack(),cardDTO.getDefense(),cardDTO.getPlayerId(),cardDTO.getPrice(),cardDTO.getIsToSell());
+		BeanUtils.copyProperties(vehicleDTO, vehicle);
+		return vehicle;
+	}
+	
+    
+//    public VehicleDTO addVehicle(String teamuuid, VehicleDTO vehicleDTO) {
+//    	//Vehicle vehiclecreate =  this.VehicleDTOTOVehicle(vehicleDTO);
+//    	//return this.vehicleToVehicleDTO(vehiclecreate);
+//    	return 
+//    }
 
 }
